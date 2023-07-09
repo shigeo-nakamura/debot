fa481865c5dc1436a2e356882b8d7da426de77cd
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-07-09 21:02:51 +0200

    Change the config

--- src/trade/fund_configurations.rs
+++ src/trade/fund_configurations.rs
@@ -7,7 +7,7 @@ pub fn get(
     short_trade_period: usize,
     medium_trade_period: usize,
     long_trade_period: usize,
-) -> Option<(
+) -> Vec<(
     &'static str,
     TradingStrategy,
     StopLossStrategy,
@@ -19,28 +19,76 @@ pub fn get(
     f64,
 )> {
     let configs = vec![
-        (
-            "trend-follow-short-trailing",
-            TradingStrategy::TrendFollowing,
-            StopLossStrategy::TrailingStop,
-            short_trade_period,
-            1.01,  // buy_signal_threshold
-            1.005, // take_profit_threshold
-            0.95,  // loss_cut_threshold
-            10.0,  // initial_score
-            1.0,   // 1 day
-        ),
-        (
-            "trend-follow-short-fixed",
-            TradingStrategy::TrendFollowing,
-            StopLossStrategy::FixedThreshold,
-            short_trade_period,
-            1.01,  // buy_signal_threshold
-            1.005, // take_profit_threshold
-            0.95,  // loss_cut_threshold
-            10.0,  // initial_score
-            1.0,   // 1 day
-        ),
+        vec![
+            (
+                "trend-follow-short-trailing",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::TrailingStop,
+                short_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+            (
+                "trend-follow-medium-trailing",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::TrailingStop,
+                medium_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+            (
+                "trend-follow-long-trailing",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::TrailingStop,
+                long_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+        ],
+        vec![
+            (
+                "trend-follow-short-fixed",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::FixedThreshold,
+                short_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+            (
+                "trend-follow-medium-fixed",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::FixedThreshold,
+                medium_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+            (
+                "trend-follow-long-fixed",
+                TradingStrategy::TrendFollowing,
+                StopLossStrategy::FixedThreshold,
+                long_trade_period,
+                1.01, // buy_signal_threshold
+                1.01, // take_profit_threshold
+                0.95, // loss_cut_threshold
+                10.0, // initial_score
+                1.0,  // 1 day
+            ),
+        ],
     ];
-    configs.get(index).cloned()
+    configs[index].clone()
 }
