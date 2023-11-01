625859a308d41cf5dd7e3b7d2f0fb478413c30df
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-02 00:26:39 +0100

    Add more tokens

--- src/trade/fund_config.rs
+++ src/trade/fund_config.rs
@@ -5,35 +5,53 @@ pub fn get() -> Vec<(String, TradingStrategy, f64, f64)> {
         (
             "BTC-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
-            200.0, // initial amount(in USDC)
+            500.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
         (
             "ETH-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
-            200.0, // initial amount(in USDC)
+            500.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
         (
             "SOL-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
-            200.0, // initial amount(in USDC)
+            300.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
         (
-            "XRP-USDC".to_owned(),
+            "MATIC-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
             200.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
         (
-            "BNB-USDC".to_owned(),
+            "LTC-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "1000PEPE-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
             200.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
         (
             "LINK-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "DOGE-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "XRP-USDC".to_owned(),
             TradingStrategy::TrendFollowing,
             200.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
