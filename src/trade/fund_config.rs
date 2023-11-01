fd1aabded9d3f43ed286b7007b612af6b3471daa
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-01 22:25:29 +0100

    Add more tokens to trade

--- src/trade/fund_config.rs
+++ src/trade/fund_config.rs
@@ -8,35 +8,35 @@ pub fn get() -> Vec<(String, TradingStrategy, f64, f64)> {
             200.0, // initial amount(in USDC)
             100.0, // amount(in USDC) per trading
         ),
-        // (
-        //     "ETH-USDC".to_owned(),
-        //     TradingStrategy::TrendFollowing,
-        //     200.0, // initial amount(in USDC)
-        //     100.0, // amount(in USDC) per trading
-        // ),
-        // (
-        //     "SOL-USDC".to_owned(),
-        //     TradingStrategy::TrendFollowing,
-        //     200.0, // initial amount(in USDC)
-        //     100.0, // amount(in USDC) per trading
-        // ),
-        // (
-        //     "XRP-USDC".to_owned(),
-        //     TradingStrategy::TrendFollowing,
-        //     200.0, // initial amount(in USDC)
-        //     100.0, // amount(in USDC) per trading
-        // ),
-        // (
-        //     "BNB-USDC".to_owned(),
-        //     TradingStrategy::TrendFollowing,
-        //     200.0, // initial amount(in USDC)
-        //     100.0, // amount(in USDC) per trading
-        // ),
-        // (
-        //     "LINK-USDC".to_owned(),
-        //     TradingStrategy::TrendFollowing,
-        //     200.0, // initial amount(in USDC)
-        //     100.0, // amount(in USDC) per trading
-        // ),
+        (
+            "ETH-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "SOL-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "XRP-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "BNB-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
+        (
+            "LINK-USDC".to_owned(),
+            TradingStrategy::TrendFollowing,
+            200.0, // initial amount(in USDC)
+            100.0, // amount(in USDC) per trading
+        ),
     ]
 }
