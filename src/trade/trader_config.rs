afd0adaeaead6d98f069e3e636cb68309047a444
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-02 09:03:37 +0100

    Change the trader config

--- src/trade/trader_config.rs
+++ src/trade/trader_config.rs
@@ -1,6 +1,6 @@
 use super::derivative_trader::TradingPeriod;
 
 pub fn get() -> Vec<(TradingPeriod, String)> {
-    let configs = vec![(TradingPeriod::new(1, 4, 24), "apex".to_owned())];
+    let configs = vec![(TradingPeriod::new(0.1, 0.4, 2.4), "apex".to_owned())];
     configs
 }
