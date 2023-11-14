4caae364c296c53148d2ac2521ff703758070603
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-14 19:56:09 +0100

    Treat as hex if a strings starts with 0x

--- scripts/encrypt.py
+++ scripts/encrypt.py
@@ -9,9 +9,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Encrypt data using an AES key.')
 
 # Define arguments
-parser.add_argument('aes_key', type=str, help='The plaintext data key you got from AWS KMS')
-parser.add_argument('data_to_encrypt', type=str, help='The data you want to encrypt')
-parser.add_argument('--hex', action='store_true', help='Indicate that the data to encrypt is in hexadecimal format')
+parser.add_argument('aes_key', type=str,
+                    help='The plaintext data key you got from AWS KMS')
+parser.add_argument('data_to_encrypt', type=str,
+                    help='The data you want to encrypt')
+parser.add_argument('--hex', action='store_true',
+                    help='Indicate that the data to encrypt is in hexadecimal format')
 
 # Parse arguments
 args = parser.parse_args()
@@ -19,9 +22,10 @@ args = parser.parse_args()
 # This is the plaintext data key you got from AWS KMS
 aes_key = base64.b64decode(args.aes_key)
 
-# Convert the data to bytes. If --hex is provided, treat it as a hex string.
-if args.hex:
-    data = bytes.fromhex(args.data_to_encrypt)
+# Convert the data to bytes. If data starts with '0x' or --hex is provided, treat it as a hex string.
+if args.data_to_encrypt.startswith('0x') or args.hex:
+    data = bytes.fromhex(args.data_to_encrypt[2:] if args.data_to_encrypt.startswith(
+        '0x') else args.data_to_encrypt)
 else:
     data = args.data_to_encrypt.encode()
 
