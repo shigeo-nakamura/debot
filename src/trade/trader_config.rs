7ec3f07185df7e695b3155dfeb1526d39f7264f8
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-17 00:15:32 +0100

    Fix trading interval

--- src/trade/trader_config.rs
+++ src/trade/trader_config.rs
@@ -1,5 +1,5 @@
 use super::derivative_trader::SampleInterval;
 
 pub fn get() -> (usize, SampleInterval, String) {
-    (1, SampleInterval::new(1, 3), "mufex".to_owned())
+    (5, SampleInterval::new(1, 3), "mufex".to_owned())
 }
