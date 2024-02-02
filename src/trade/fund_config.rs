f3f20c02f49cd4d46866bd81bcaa0949d4cd5d6c
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-02-02 17:00:32 +0100

    Update the fund config

--- src/trade/fund_config.rs
+++ src/trade/fund_config.rs
@@ -12,37 +12,37 @@ pub fn get(dex_name: &str, strategy: &TradingStrategy) -> Vec<(String, f64)> {
             (
                 RABBITX_TOKEN_LIST[0].to_owned(), // BTC
                 TradingStrategy::RangeGrid,
-                2500.0, // initial amount(in USD)
+                500.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[0].to_owned(), // BTC
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                500.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[1].to_owned(), // ETH
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                0.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[2].to_owned(), // SOL
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                0.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[3].to_owned(), // SUI
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                0.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[4].to_owned(), // APT
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                0.0, // initial amount(in USD)
             ),
             (
                 RABBITX_TOKEN_LIST[5].to_owned(), // ARB
                 TradingStrategy::TrendFollow,
-                2500.0, // initial amount(in USD)
+                0.0, // initial amount(in USD)
             ),
         ];
 
