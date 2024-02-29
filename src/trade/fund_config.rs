f6df20eca1191f1771ecb86cb32c8e6367201569
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-02-29 06:31:02 +0100

    Update the fund config

--- src/trade/fund_config.rs
+++ src/trade/fund_config.rs
@@ -12,50 +12,50 @@ pub fn get(dex_name: &str, strategy: &TradingStrategy) -> Vec<(String, f64, f64,
             (
                 RABBITX_TOKEN_LIST[0].to_owned(), // BTC
                 TradingStrategy::TrendFollow,
-                500.0, // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
             (
                 RABBITX_TOKEN_LIST[1].to_owned(), // ETH
                 TradingStrategy::TrendFollow,
-                0.0,   // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
             (
                 RABBITX_TOKEN_LIST[2].to_owned(), // SOL
                 TradingStrategy::TrendFollow,
-                0.0,   // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
             (
                 RABBITX_TOKEN_LIST[3].to_owned(), // SUI
                 TradingStrategy::TrendFollow,
-                0.0,   // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
             (
                 RABBITX_TOKEN_LIST[4].to_owned(), // APT
                 TradingStrategy::TrendFollow,
-                0.0,   // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
             (
                 RABBITX_TOKEN_LIST[5].to_owned(), // ARB
                 TradingStrategy::TrendFollow,
-                0.0,   // initial amount(in USD)
-                0.02,  // position size ration (2%)
-                1.2,   // risk reward
-                0.001, // loss cut ration (0.1%)
+                100.0, // initial amount(in USD)
+                0.1,   // position size ration
+                2.0,   // risk reward
+                0.005, // loss cut ration
             ),
         ];
 
