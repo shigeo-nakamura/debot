5bd3c4c5ade59e94a3963a74214c90152b335038
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-04-05 11:52:05 +0200

    Replace TrendFollow with TrendFollowLong and Short

--- src/config.rs
+++ src/config.rs
@@ -127,7 +127,8 @@ pub fn get_config_from_env() -> Result<EnvConfig, ConfigError> {
     let leverage = get_env_var("LEVERAGE", "5")?;
 
     let strategy = match env::var("TRADING_STRATEGY").unwrap_or_default().as_str() {
-        "trendfollow" => Some(TradingStrategy::TrendFollow),
+        "trendfollow-long" => Some(TradingStrategy::TrendFollowLong),
+        "trendfollow-short" => Some(TradingStrategy::TrendFollowShort),
         "rebalance" => Some(TradingStrategy::Rebalance),
         "marketmake" => Some(TradingStrategy::MarketMake),
         &_ => None,
